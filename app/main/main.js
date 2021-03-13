1a69259ec51e5c7fe784e636b7998b7831c716fa
_ihhu <v.ihhu@qq.com>
2021-03-13 22:01:46 +0800

    升级依赖版本

--- app/main/main.js
+++ app/main/main.js
@@ -1,1 +1,149 @@
-(()=>{"use strict";function e(){new a.BrowserWindow({width:800,height:600,minWidth:800,minHeight:600,webPreferences:{nodeIntegration:!0,enableRemoteModule:!0,webSecurity:!1}}).loadURL(new URL("./index.html",i).toString())}var r,o={n:e=>{var r=e&&e.__esModule?()=>e.default:()=>e;return o.d(r,{a:r}),r},d:(e,r)=>{for(var n in r)o.o(r,n)&&!o.o(e,n)&&Object.defineProperty(e,n,{enumerable:!0,get:r[n]})},o:(e,r)=>Object.prototype.hasOwnProperty.call(e,r)};const n=require("path");r=o.n(n);const t=require("url"),a=require("electron");console.log("electron main process running."),console.log("electron main process argvs",process.argv);let i=(0,t.pathToFileURL)(r().join(process.cwd(),"./resources/app.asar/renderer/")).toString();a.app.on("window-all-closed",(()=>{"darwin"!==process.platform&&a.app.quit()})),a.app.on("activate",(()=>{0===a.BrowserWindow.getAllWindows().length&&e()})),a.app.on("ready",(()=>{e()}))})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((module) => {
+
+module.exports = require("path");;
+
+/***/ }),
+/* 2 */
+/***/ ((module) => {
+
+module.exports = require("url");;
+
+/***/ }),
+/* 3 */
+/***/ ((module) => {
+
+module.exports = require("electron");;
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var path__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var path__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(path__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var url__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2);
+/* harmony import */ var url__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(url__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var electron__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(3);
+/* harmony import */ var electron__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(electron__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+console.log("electron main process running.");
+console.log("electron main process argvs", process.argv);
+const RENDERER_PATH = "./resources/app.asar/renderer/";
+let urlOrigin = (0,url__WEBPACK_IMPORTED_MODULE_1__.pathToFileURL)(path__WEBPACK_IMPORTED_MODULE_0___default().join(process.cwd(), RENDERER_PATH)).toString();
+
+if (true) {
+  urlOrigin = `${{"host":"localhost","port":8080,"protocol":"http"}.protocol}://${{"host":"localhost","port":8080,"protocol":"http"}.host}:${{"host":"localhost","port":8080,"protocol":"http"}.port}/`;
+  console.log("electron main process devServer", urlOrigin, new URL("./", urlOrigin).toString());
+}
+
+function createWindow() {
+  const win = new electron__WEBPACK_IMPORTED_MODULE_2__.BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 800,
+    minHeight: 600,
+    // frame : false,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      webSecurity: false
+    }
+  });
+  win.loadURL(new URL("./index.html", urlOrigin).toString());
+}
+
+electron__WEBPACK_IMPORTED_MODULE_2__.app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    electron__WEBPACK_IMPORTED_MODULE_2__.app.quit();
+  }
+});
+electron__WEBPACK_IMPORTED_MODULE_2__.app.on('activate', () => {
+  if (electron__WEBPACK_IMPORTED_MODULE_2__.BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+}); // app.whenReady().then(createWindow)
+
+electron__WEBPACK_IMPORTED_MODULE_2__.app.on('ready', () => {
+  createWindow();
+}); // 启用热更新
+
+if (false) {}
+})();
+
+/******/ })()
+;
